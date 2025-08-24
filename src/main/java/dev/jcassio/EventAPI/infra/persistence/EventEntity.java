e2e43f29dcb54d2ee6c09e243c2ec478bc2f9d5b
JCassio <josecassio2013@gmail.com>
2025-08-24 13:40:24 -0300

    feat(infra): create EventEntity

--- src/main/java/dev/jcassio/EventAPI/infra/persistence/EventEntity.java
+++ src/main/java/dev/jcassio/EventAPI/infra/persistence/EventEntity.java
@@ -1,4 +1,31 @@
 package dev.jcassio.EventAPI.infra.persistence;
 
+import dev.jcassio.EventAPI.core.enums.EventType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "events")
 public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String identifier;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String location;
+    private String organizer;
+    private Integer capacity;
+    @Enumerated(EnumType.STRING)
+    private EventType type;
 }
