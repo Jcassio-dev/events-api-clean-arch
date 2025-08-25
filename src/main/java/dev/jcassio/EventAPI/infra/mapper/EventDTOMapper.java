cd535950345c3fb00fd70103e5510ab678d91b9d
JCassio <josecassio2013@gmail.com>
2025-08-25 19:15:04 -0300

    feat(infra): implements convertion method into eventdtomapper

--- src/main/java/dev/jcassio/EventAPI/infra/mapper/EventDTOMapper.java
+++ src/main/java/dev/jcassio/EventAPI/infra/mapper/EventDTOMapper.java
@@ -1,4 +1,37 @@
 package dev.jcassio.EventAPI.infra.mapper;
 
+import dev.jcassio.EventAPI.core.entities.Event;
+import dev.jcassio.EventAPI.infra.dto.EventDTO;
+
 public class EventDTOMapper {
+    public EventDTO toDTO(Event event){
+        return new EventDTO(
+                event.id(),
+                event.name(),
+                event.description(),
+                event.identifier(),
+                event.startDate(),
+                event.endDate(),
+                event.location(),
+                event.organizer(),
+                event.capacity(),
+                event.type()
+        );
+    }
+
+
+    public Event toEntity(EventDTO eventDTO){
+        return new Event(
+                eventDTO.id(),
+                eventDTO.name(),
+                eventDTO.description(),
+                eventDTO.identifier(),
+                eventDTO.startDate(),
+                eventDTO.endDate(),
+                eventDTO.location(),
+                eventDTO.organizer(),
+                eventDTO.capacity(),
+                eventDTO.type()
+        );
+    }
 }
