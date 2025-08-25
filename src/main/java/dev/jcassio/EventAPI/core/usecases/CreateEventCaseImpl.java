8b5b348c88e6cadd3692b529fea6b4f5fdeb8164
JCassio <josecassio2013@gmail.com>
2025-08-25 19:34:48 -0300

    chore(core): remove @service annotation from create event use-case

--- src/main/java/dev/jcassio/EventAPI/core/usecases/CreateEventCaseImpl.java
+++ src/main/java/dev/jcassio/EventAPI/core/usecases/CreateEventCaseImpl.java
@@ -1,9 +1,7 @@
 package dev.jcassio.EventAPI.core.usecases;
 
 import dev.jcassio.EventAPI.core.entities.Event;
-import org.springframework.stereotype.Service;
 
-@Service
 public class CreateEventCaseImpl implements CreateEventCase {
     @Override
     public Event execute(Event event) {
