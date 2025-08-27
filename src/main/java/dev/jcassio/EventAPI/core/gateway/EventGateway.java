05ce9f56cfcc633da693d58672db35018ed87e6b
JCassio <josecassio2013@gmail.com>
2025-08-27 19:38:32 -0300

    feat(core/infra): create EventGateway interface on core and EventRepositoryGateway on infra to implements core interface

---
+++ src/main/java/dev/jcassio/EventAPI/core/gateway/EventGateway.java
@@ -0,0 +1,4 @@
+package dev.jcassio.EventAPI.core.gateway;
+
+public interface EventGateway {
+}
---
+++ src/main/java/dev/jcassio/EventAPI/infra/gateway/EventRepositoryGateway.java
@@ -0,0 +1,6 @@
+package dev.jcassio.EventAPI.infra.gateway;
+
+import dev.jcassio.EventAPI.core.gateway.EventGateway;
+
+public class EventRepositoryGateway implements EventGateway {
+}
