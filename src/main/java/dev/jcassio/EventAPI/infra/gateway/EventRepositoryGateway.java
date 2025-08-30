6e6f45033aeaf2d1a568854b2faa39b33abf756d
JCassio <josecassio2013@gmail.com>
2025-08-29 23:43:32 -0300

    feat(infra): implements event gateway methods

--- src/main/java/dev/jcassio/EventAPI/infra/gateway/EventRepositoryGateway.java
+++ src/main/java/dev/jcassio/EventAPI/infra/gateway/EventRepositoryGateway.java
@@ -1,6 +1,26 @@
 package dev.jcassio.EventAPI.infra.gateway;
 
+import dev.jcassio.EventAPI.core.entities.Event;
 import dev.jcassio.EventAPI.core.gateway.EventGateway;
+import dev.jcassio.EventAPI.infra.mapper.EventEntityMapper;
+import dev.jcassio.EventAPI.infra.persistence.EventEntity;
+import dev.jcassio.EventAPI.infra.persistence.EventRepository;
 
 public class EventRepositoryGateway implements EventGateway {
+
+    private final EventRepository eventRepository;
+    private final EventEntityMapper eventEntityMapper = new EventEntityMapper();
+    public EventRepositoryGateway(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Event createEvent(Event event){
+        EventEntity mappedEvent = eventEntityMapper.toEntity(event);
+
+        EventEntity createdEvent = eventRepository.save(mappedEvent);
+
+
+        return eventEntityMapper.toDomain(createdEvent);
+    }
 }
